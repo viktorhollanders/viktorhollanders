a737da062be4c6c4fea0fbf54dbe66944fd8fd88
viktorhollanders <viktorhollanders@hey.com>
2024-06-03 18:01:03 +0000

    Dynamically add content to modal

--- public/script.js
+++ public/script.js
@@ -16,18 +16,37 @@ const getAllProjects = async () => {
 const projectsContainer = document.querySelector('#project-container');
 console.log(projectsContainer);
 
-const projectModal = (e) => {
-  console.log(e.target.id);
+const modal = document.querySelector('.modal');
+let modalTitle = modal.querySelector('.project-title');
+let modalShape = modal.querySelector('.project-shape');
+let modalRole = modal.querySelector('.project-role');
+let modalDescription = modal.querySelector('.project-description');
+let modalWebsite = modal.querySelector('.project-website');
+let modalGithub = modal.querySelector('.project-github');
+
+const onClick = (e) => {
+  const projectId = e.target.id;
+  const project = projects.find((project) => project.id === parseInt(projectId));
+
+  if (project) {
+    console.log(project);
+    modalTitle.textContent = project.name;
+    modalShape.style.backgroundColor = `rgb(${project.color})`;
+    modalRole.textContent = project.role;
+    modalDescription.textContent = project.description;
+    modalWebsite.href = project.website;
+    modalGithub.href = project.github;
+  }
 };
 
 getAllProjects().then(() => {
   projects.forEach((project) => {
     const newProject = document.createElement('h3');
     newProject.textContent = project.name;
-    newProject.style.color = project.color;
+    newProject.style.color = `rgb(${project.color})`;
     newProject.classList.add('project');
-    newProject.id = `pro${project.id}`;
-    newProject.addEventListener('click', projectModal);
+    newProject.id = project.id;
+    newProject.addEventListener('click', onClick);
     projectsContainer.appendChild(newProject);
   });
 });
